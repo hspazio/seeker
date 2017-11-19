86871df485a57873fb5a57cd77fa26381b716222
Fabio Pitino <pitinofabio@gmail.com>
2017-11-19 22:22:44 +0000

    move parsers and glassdoor into separate packages

--- glassdoor/glassdoor.go
+++ glassdoor/glassdoor.go
@@ -1,4 +1,4 @@
-package main
+package glassdoor
 
 import (
 	"encoding/json"
@@ -6,13 +6,13 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
-	"os"
 )
 
 type Client struct {
-	id  string
-	key string
+	Id  string
+	Key string
 }
+
 type Employer struct {
 	Id                            int    `json:"id"`
 	Name                          string `json:"name"`
@@ -78,15 +78,5 @@ func (c *Client) SearchEmployer(employer string) (Employer, error) {
 
 func (c *Client) buildUrl(action, q string) string {
 	host := "http://api.glassdoor.com/api/api.htm"
-	return fmt.Sprintf("%s?=1&format=json&t.p=%s&t.k=%s&action=%s&q=%s&userip=%s&useragent=%s", host, c.id, c.key, action, q, "192.168.1.1", "Mozilla")
-}
-
-func main() {
-	gd := Client{id: os.Getenv("GLASSDOOR_ID"), key: os.Getenv("GLASSDOOR_KEY")}
-	empl, err := gd.SearchEmployer("Google")
-	if err != nil {
-		fmt.Println(err)
-	} else {
-		fmt.Println(empl)
-	}
+	return fmt.Sprintf("%s?=1&format=json&t.p=%s&t.k=%s&action=%s&q=%s&userip=%s&useragent=%s", host, c.Id, c.Key, action, q, "192.168.1.1", "Mozilla")
 }
